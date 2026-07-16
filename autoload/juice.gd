44f7a99a0887394917f25e220223f55e64dfc7bc
Roman Valero <roman.valero@tafitech.io>
2026-07-16 12:21:25 -0500

    feat: Implement camera shake effect in juice.gd for enhanced visual feedback

--- autoload/juice.gd
+++ autoload/juice.gd
@@ -1,7 +1,16 @@
 extends Node
 
 func shake(intensity: float) -> void:
-	pass
+	var camera := get_viewport().get_camera_2d()
+	if not camera:
+		return
+	var tween := create_tween()
+	var shake_duration := 0.2
+	var steps := 6
+	for i in steps:
+		var offset := Vector2(randf_range(-intensity, intensity), randf_range(-intensity, intensity))
+		tween.tween_property(camera, "offset", offset, shake_duration / steps)
+	tween.tween_property(camera, "offset", Vector2.ZERO, shake_duration / steps)
 
 func hit_stop(duration: float) -> void:
 	pass
